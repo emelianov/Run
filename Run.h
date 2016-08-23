5a9895fea718ad2e0161a16b0cb9405a7f8702ac
Alexander Emelianov <a.m.emelianov@gmail.com>
2016-08-23 23:19:00 +0500

    2016.1

--- Run.h
+++ Run.h
@@ -1,34 +1,72 @@
 /////////////////////////////////////////////////////////
-// Run
-// Arduino cooperative multitask library
-// (c)2015, Alexander Emelianov (a.m.emelainov@gmail.com)
+// Run 2016.1
+// Arduino simple cooperative multitask library
+// (c)2016, Alexander Emelianov (a.m.emelainov@gmail.com)
+//
+// Usage:
+// uint32_t taskToRun() {
+//   //DO SOMETHING
+//  return delayToNextRun_OR_0_ToRemoveTask
+// }
+// void setup() {
+//  addTask(taskToRun);
+// }
+// void loop() {
+//  TASKEXEC //Macro to run tasks
+// }
 
-#ifndef Run_h
- #define Run_h
+#pragma once
 
- #include <inttypes.h>
- #if ARDUINO >= 100
-  #include "Arduino.h"       // for delayMicroseconds, digitalPinToBitMask, etc
-  #include "WString.h"
- #else
-  #include "WProgram.h"      // for delayMicroseconds
-  #include "WString.h"
- #endif
+#include <inttypes.h>
+#include "Arduino.h"
 
- #ifndef MAX_TASKS
- #define MAX_TASKS      32
- #endif
-
- typedef unsigned long (*task)();
-
- class Run {
-  int count = 0;
-  task tasks[MAX_TASKS];
-  unsigned long nextRun[MAX_TASKS];
-  public:
-  byte addTask(task thread);
-  bool delTask(byte id);
-  void execute();
- };
-
+#ifndef MAX_TASKS
+ #define MAX_TASKS      16
 #endif
+#define TASKEXEC for(uint8_t i = 0; i < taskCount; i++) {if (millis() - taskTasks[i].lastRun > taskTasks[i].delay) {taskTasks[i].lastRun = millis(); taskTasks[i].delay = taskTasks[i].thread();if (taskTasks[i].delay == 0) if (taskDel(i)) break;}}
+
+typedef uint32_t (*task)();
+struct taskThread {
+ task thread;
+ uint32_t lastRun;
+ uint32_t delay;
+};
+
+uint8_t taskCount = 0;
+taskThread taskTasks[MAX_TASKS];
+
+uint8_t taskAddWithDelay(task thread, uint32_t delay) {
+ taskTasks[taskCount].thread    = thread;
+ taskTasks[taskCount].lastRun  = millis();
+ taskTasks[taskCount].delay = delay;
+ taskCount++;
+ return taskCount - 1;
+}
+
+uint8_t taskAdd(task thread) {
+ return taskAddWithDelay(thread, 1);
+}
+
+bool taskDel(byte id) {
+ if (id < taskCount) {
+  memcpy(&taskTasks[id], &taskTasks[id + 1], (taskCount - id - 1) * sizeof(taskThread));
+  taskCount--;
+  return true;
+ } else {
+  return false;
+ }
+}
+/*
+void taskExec() {
+ for(uint8_t i = 0; i < taskCount; i++) {
+  if (millis() - taskLastRun[i] > taskDelay[i]) {
+   taskLastRun[i] = millis(); taskDelay[i] = taskTasks[i]();
+   if (taskDelay[i] == 0) {
+    if (taskDel(i)) {
+     break;
+    }
+   }
+  }
+ }
+}
+*/
