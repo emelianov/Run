6e15efae29cdc839bc3ed23e67848829c1c20489
Alexander Emelianov <a.m.emelianov@gmail.com>
2016-09-13 11:45:23 +0500

    2016.1.2

--- Run.h
+++ Run.h
@@ -1,19 +1,8 @@
 /////////////////////////////////////////////////////////
-// Run 2016.1
+// Run 2016.1.2
 // Arduino simple cooperative multitask library
 // (c)2016, Alexander Emelianov (a.m.emelainov@gmail.com)
 //
-// Usage:
-// uint32_t taskToRun() {
-//   //DO SOMETHING
-//  return delayToNextRun_OR_0_ToRemoveTask
-// }
-// void setup() {
-//  addTask(taskToRun);
-// }
-// void loop() {
-//  TASKEXEC //Macro to run tasks
-// }
 
 #pragma once
 
@@ -23,7 +12,8 @@
 #ifndef MAX_TASKS
  #define MAX_TASKS      16
 #endif
-#define TASKEXEC for(uint8_t i = 0; i < taskCount; i++) {if (millis() - taskTasks[i].lastRun > taskTasks[i].delay) {taskTasks[i].lastRun = millis(); taskTasks[i].delay = taskTasks[i].thread();if (taskTasks[i].delay == 0) if (taskDel(i)) break;}}
+//#define TASKEXEC for(uint8_t i = 0; i < taskCount; i++) {if (millis() - taskTasks[i].lastRun > taskTasks[i].delay) {taskTasks[i].lastRun = millis(); taskTasks[i].delay = taskTasks[i].thread();if (taskTasks[i].delay == 0) if (taskDel(i)) break;}}
+#define TASKEXEC taskExec();
 
 typedef uint32_t (*task)();
 struct taskThread {
@@ -35,15 +25,19 @@ struct taskThread {
 uint8_t taskCount = 0;
 taskThread taskTasks[MAX_TASKS];
 
-uint8_t taskAddWithDelay(task thread, uint32_t delay) {
- taskTasks[taskCount].thread    = thread;
- taskTasks[taskCount].lastRun  = millis();
- taskTasks[taskCount].delay = delay;
- taskCount++;
- return taskCount - 1;
+int16_t taskAddWithDelay(task thread, uint32_t delay) {
+ if (taskCount < MAX_TASKS) {
+   taskTasks[taskCount].thread    = thread;
+   taskTasks[taskCount].lastRun  = millis();
+   taskTasks[taskCount].delay = delay;
+   taskCount++;
+   return taskCount - 1;
+ } else {
+   return -1;
+ }
 }
 
-uint8_t taskAdd(task thread) {
+int16_t taskAdd(task thread) {
  return taskAddWithDelay(thread, 1);
 }
 
@@ -56,17 +50,26 @@ bool taskDel(byte id) {
   return false;
  }
 }
-/*
-void taskExec() {
- for(uint8_t i = 0; i < taskCount; i++) {
-  if (millis() - taskLastRun[i] > taskDelay[i]) {
-   taskLastRun[i] = millis(); taskDelay[i] = taskTasks[i]();
-   if (taskDelay[i] == 0) {
-    if (taskDel(i)) {
-     break;
+
+bool taskDel(task thread) {
+  for (uint8_t i = 0; i < taskCount; i++) {
+    if (taskTasks[i].thread = thread) {
+      taskDel(i);
+      break;
     }
-   }
   }
- }
 }
-*/
+
+void taskExec() {
+  for(uint8_t i = 0; i < taskCount; i++) {
+    if (millis() - taskTasks[i].lastRun > taskTasks[i].delay) {
+      taskTasks[i].lastRun = millis();
+      taskTasks[i].delay = taskTasks[i].thread();
+      if (taskTasks[i].delay == 0) {
+        if (taskDel(i)) {
+          break;
+        }
+      }
+    }
+  }
+}
