e039ea270bc761595a966eb762da0a533ee6e3d9
Alexander Emelianov <a.m.emelianov@gmail.com>
2017-01-17 08:46:50 +0500

    Semaphore thread support

--- Run.h
+++ Run.h
@@ -1,11 +1,12 @@
 /////////////////////////////////////////////////////////
-// Run 2016.1.2
+// Run 2016.2
 // Arduino simple cooperative multitask library
-// (c)2016, Alexander Emelianov (a.m.emelianov@gmail.com)
+// (c)2017, Alexander Emelianov (a.m.emelianov@gmail.com)
 //
 
 #pragma once
 
+#define RUN_NEVER 0xFFFFFFFF
 #ifndef MAX_TASKS
  #define MAX_TASKS      16
 #endif
@@ -17,20 +18,26 @@ struct taskThread {
  task thread;
  uint32_t lastRun;
  uint32_t delay;
+ bool *signal;
 };
 
 uint8_t taskCount = 0;
 taskThread taskTasks[MAX_TASKS];
 
-int16_t taskAddWithDelay(task thread, uint32_t delay) {
+int16_t taskAddWithDelay(task thread, uint32_t delay, bool *signal = NULL) {
  if (taskCount < MAX_TASKS) {
    taskTasks[taskCount].thread    = thread;
    taskTasks[taskCount].lastRun  = millis();
    taskTasks[taskCount].delay = delay;
+   taskTasks[taskCount].signal = signal;
    taskCount++;
    return taskCount - 1;
  }
  return -1;
+}
+
+int16_t taskAddWithSemaphore(task thread, bool * signal) {
+ return taskAddWithDelay(thread, RUN_NEVER, signal);
 }
 
 int16_t taskAdd(task thread) {
@@ -48,7 +55,7 @@ bool taskDel(uint8_t id) {
 
 bool taskDel(task thread) {
   for (uint8_t i = 0; i < taskCount; i++) {
-    if (taskTasks[i].thread = thread) {
+    if (taskTasks[i].thread == thread) {
       return taskDel(i);
     }
   }
@@ -57,13 +64,17 @@ bool taskDel(task thread) {
 
 void taskExec() {
   for(uint8_t i = 0; i < taskCount; i++) {
-    if (millis() - taskTasks[i].lastRun > taskTasks[i].delay) {
+    if (millis() - taskTasks[i].lastRun > taskTasks[i].delay || \
+    	(taskTasks[i].signal != NULL && &taskTasks[i].signal)) {
       taskTasks[i].lastRun = millis();
       taskTasks[i].delay = taskTasks[i].thread();
       if (taskTasks[i].delay == 0) {
         if (taskDel(i)) {
-          break;
+          i--;
         }
+      }
+      if (taskTasks[i].signal != NULL) {
+      	&taskTasks[i].signal = false;
       }
     }
   }
